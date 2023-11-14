0e68721e79508126bcd63ba4e35dff1b5dafbbb7
ojongclinton <sheyenoh123@gmail.com>
2023-11-14 19:49:09 +0100

    logo

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,104 +1,104 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { AiOutlineSearch, AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai';
-import { dropdownMenu } from '../../data/headerData';
-import commonContext from '../../contexts/common/commonContext';
-import cartContext from '../../contexts/cart/cartContext';
-import AccountForm from '../form/AccountForm';
-import SearchBar from './SearchBar';
-
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  AiOutlineSearch,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+} from "react-icons/ai";
+import { dropdownMenu } from "../../data/headerData";
+import commonContext from "../../contexts/common/commonContext";
+import cartContext from "../../contexts/cart/cartContext";
+import AccountForm from "../form/AccountForm";
+import SearchBar from "./SearchBar";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
+  const { formUserInfo, toggleForm, toggleSearch } = useContext(commonContext);
+  const { cartItems } = useContext(cartContext);
+  const [isSticky, setIsSticky] = useState(false);
 
-    const { formUserInfo, toggleForm, toggleSearch } = useContext(commonContext);
-    const { cartItems } = useContext(cartContext);
-    const [isSticky, setIsSticky] = useState(false);
+  // handle the sticky-header
+  useEffect(() => {
+    const handleIsSticky = () =>
+      window.scrollY >= 50 ? setIsSticky(true) : setIsSticky(false);
 
+    window.addEventListener("scroll", handleIsSticky);
 
-    // handle the sticky-header
-    useEffect(() => {
-        const handleIsSticky = () => window.scrollY >= 50 ? setIsSticky(true) : setIsSticky(false);
+    return () => {
+      window.removeEventListener("scroll", handleIsSticky);
+    };
+  }, [isSticky]);
 
-        window.addEventListener('scroll', handleIsSticky);
+  const cartQuantity = cartItems.length;
 
-        return () => {
-            window.removeEventListener('scroll', handleIsSticky);
-        };
-    }, [isSticky]);
+  return (
+    <>
+      <header id="header" className={isSticky ? "sticky" : ""}>
+        <div className="container">
+          <div className="navbar">
+            <div style={{ display: "flex", gap: "10px" }}>
+              <h2 className="nav_logo">
+                <Link to="/">X-Beat - </Link>
+              </h2>
+              <div>
+                <img src={logo} alt="logo" style={{ height: "30px" }} />
+              </div>
+            </div>
+            <nav className="nav_actions">
+              <div className="search_action">
+                <span onClick={() => toggleSearch(true)}>
+                  <AiOutlineSearch />
+                </span>
+                <div className="tooltip">Search</div>
+              </div>
 
+              <div className="cart_action">
+                <Link to="/cart">
+                  <AiOutlineShoppingCart />
+                  {cartQuantity > 0 && (
+                    <span className="badge">{cartQuantity}</span>
+                  )}
+                </Link>
+                <div className="tooltip">Cart</div>
+              </div>
 
-    const cartQuantity = cartItems.length;
-
-
-    return (
-        <>
-            <header id="header" className={isSticky ? 'sticky' : ''}>
-                <div className="container">
-                    <div className="navbar">
-                        <h2 className="nav_logo">
-                            <Link to="/">X-Beat</Link>
-                        </h2>
-                        <nav className="nav_actions">
-                            <div className="search_action">
-                                <span onClick={() => toggleSearch(true)}>
-                                    <AiOutlineSearch />
-                                </span>
-                                <div className="tooltip">Search</div>
-                            </div>
-
-                            <div className="cart_action">
-                                <Link to="/cart">
-                                    <AiOutlineShoppingCart />
-                                    {
-                                        cartQuantity > 0 && (
-                                            <span className="badge">{cartQuantity}</span>
-                                        )
-                                    }
-                                </Link>
-                                <div className="tooltip">Cart</div>
-                            </div>
-
-                            <div className="user_action">
-                                <span>
-                                    <AiOutlineUser />
-                                </span>
-                                <div className="dropdown_menu">
-                                    <h4>Hello! {formUserInfo && <Link to="*">&nbsp;{formUserInfo}</Link>}</h4>
-                                    <p>Access account and manage orders</p>
-                                    {
-                                        !formUserInfo && (
-                                            <button
-                                                type="button"
-                                                onClick={() => toggleForm(true)}
-                                            >
-                                                Login / Signup
-                                            </button>
-                                        )
-                                    }
-                                    <div className="separator"></div>
-                                    <ul>
-                                        {
-                                            dropdownMenu.map(item => {
-                                                const { id, link, path } = item;
-                                                return (
-                                                    <li key={id}>
-                                                        <Link to={path}>{link}</Link>
-                                                    </li>
-                                                );
-                                            })
-                                        }
-                                    </ul>
-                                </div>
-                            </div>
-                        </nav>
-                    </div>
+              <div className="user_action">
+                <span>
+                  <AiOutlineUser />
+                </span>
+                <div className="dropdown_menu">
+                  <h4>
+                    Hello!{" "}
+                    {formUserInfo && <Link to="*">&nbsp;{formUserInfo}</Link>}
+                  </h4>
+                  <p>Access account and manage orders</p>
+                  {!formUserInfo && (
+                    <button type="button" onClick={() => toggleForm(true)}>
+                      Login / Signup
+                    </button>
+                  )}
+                  <div className="separator"></div>
+                  <ul>
+                    {dropdownMenu.map((item) => {
+                      const { id, link, path } = item;
+                      return (
+                        <li key={id}>
+                          <Link to={path}>{link}</Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
                 </div>
-            </header>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </header>
 
-            <SearchBar />
-            <AccountForm />
-        </>
-    );
+      <SearchBar />
+      <AccountForm />
+    </>
+  );
 };
 
 export default Header;
