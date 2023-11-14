556db80d751e87ced34865a7a87cabf9fdc4f3f3
ojongclinton <sheyenoh123@gmail.com>
2023-11-14 19:50:21 +0100

    formatiing

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -1,30 +1,35 @@
-import {FaShippingFast, FaShieldAlt, FaTags, FaCreditCard} from 'react-icons/fa';
+import {
+  FaShippingFast,
+  FaShieldAlt,
+  FaTags,
+  FaCreditCard,
+} from "react-icons/fa";
 
 const servicesData = [
-    {
-        id: 1,
-        icon: <FaShippingFast />,
-        title: "Express Delivery",
-        info: "Ships in 24 Hours",
-    },
-    {
-        id: 2,
-        icon: <FaShieldAlt />,
-        title: "Brand Warranty",
-        info: "100% Original products",
-    },
-    {
-        id: 3,
-        icon: <FaTags />,
-        title: "Exciting Deals",
-        info: "On all prepaid orders",
-    },
-    {
-        id: 4,
-        icon: <FaCreditCard />,
-        title: "Secure Payments",
-        info: "SSL / Secure сertificate",
-    },
+  {
+    id: 1,
+    icon: <FaShippingFast />,
+    title: "Express Delivery",
+    info: "Ships in 24 Hours",
+  },
+  {
+    id: 2,
+    icon: <FaShieldAlt />,
+    title: "Brand Warranty",
+    info: "100% Original products",
+  },
+  {
+    id: 3,
+    icon: <FaTags />,
+    title: "Exciting Deals",
+    info: "On all prepaid orders",
+  },
+  {
+    id: 4,
+    icon: <FaCreditCard />,
+    title: "Secure Payments",
+    info: "SSL / Secure сertificate",
+  },
 ];
 
 export default servicesData;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,34 @@
-import React from 'react';
-import HeroSlider from '../components/sliders/HeroSlider';
-import FeaturedSlider from '../components/sliders/FeaturedSlider';
-import SectionsHead from '../components/common/SectionsHead';
-import TopProducts from '../components/product/TopProducts';
-import Services from '../components/common/Services';
-
+import React from "react";
+import HeroSlider from "../components/sliders/HeroSlider";
+import FeaturedSlider from "../components/sliders/FeaturedSlider";
+import SectionsHead from "../components/common/SectionsHead";
+import TopProducts from "../components/product/TopProducts";
+import Services from "../components/common/Services";
 
 const Home = () => {
+  return (
+    <main>
+      <section id="hero">
+        <HeroSlider />
+      </section>
 
-    return (
-        <main>
-            <section id="hero">
-                <HeroSlider />
-            </section>
+      <section id="featured" className="section">
+        <div className="container">
+          <SectionsHead heading="Featured Products" />
+          <FeaturedSlider />
+        </div>
+      </section>
 
-            <section id="featured" className="section">
-                <div className="container">
-                    <SectionsHead heading="Featured Products" />
-                    <FeaturedSlider />
-                </div>
-            </section>
+      <section id="products" className="section">
+        <div className="container">
+          <SectionsHead heading="Top Products" />
+          <TopProducts />
+        </div>
+      </section>
 
-            <section id="products" className="section">
-                <div className="container">
-                    <SectionsHead heading="Top Products" />
-                    <TopProducts />
-                </div>
-            </section>
-
-            <Services />
-        </main>
-    );
+      <Services />
+    </main>
+  );
 };
 
-export default Home;;
+export default Home;
