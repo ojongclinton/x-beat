63e97af6602d36e2466bda348114235c341b28f5
ojongclinton <sheyenoh123@gmail.com>
2023-11-14 20:00:15 +0100

    pricing

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,26 +1,23 @@
-
 // Display Money in Indian Format
 export const displayMoney = (n) => {
-    const numFormat = new Intl.NumberFormat('en-IN', {
-        style: 'currency',
-        currency: 'INR',
-    });
+  const numFormat = new Intl.NumberFormat("en-Us", {
+    style: "currency",
+    currency: "XAF",
+  });
 
-    return numFormat.format(n).split('.', 1);
+  return numFormat.format(n).split(".", 1);
 };
-
 
 // Calculate Discount Percentage
 export const calculateDiscount = (discountedPrice, originalPrice) => {
-    const discountedPercent = (discountedPrice / originalPrice) * 100;
+  const discountedPercent = (discountedPrice / originalPrice) * 100;
 
-    return Math.round(discountedPercent);
+  return Math.round(discountedPercent);
 };
-
 
 // Calculate Total Amount
 export const calculateTotal = (arr) => {
-    const total = arr.reduce((accum, val) => accum + val, 0);
+  const total = arr.reduce((accum, val) => accum + val, 0);
 
-    return total;
+  return total;
 };
